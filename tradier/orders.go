181beac5f40e68665a647fbe0e18cd442543158a
Calvin Leung Huang <cleung2010@gmail.com>
2016-08-25 01:49:44 -0400

    Remove the need for isNull on orders

--- tradier/orders.go
+++ tradier/orders.go
@@ -5,15 +5,17 @@ import (
 	"time"
 )
 
-// `orders` JSON can be an object or string literal "null"
+// Orders represents the `orders` JSON object
+// If `order` is an array of objects, it can be accessed though indexing on Orders.Order
+// If `order` is a single object, it can be accessed on the zeroth element
+// If `order` is "null", Orders.Order will be empty
 type Orders struct {
-	Order  []Order `json:"order,omitempty"`
-	isNull *string
+	Order []Order `json:"order,omitempty"`
 }
 
 type orders Orders
 
-// `order` JSON can be an object or array of objects
+// Order represents the `order` JSON object
 type Order struct {
 	AvgFillPrice      *float64   `json:"avg_fill_price,omitempty"`
 	Class             *string    `json:"class,omitempty"`
@@ -30,14 +32,13 @@ type Order struct {
 	Symbol            *string    `json:"symbol,omitempty"`
 	TransactionDate   *time.Time `json:"transaction_date,omitempty"`
 	Type              *string    `json:"type,omitempty"`
-	// orderArray        `json:"-"` // Used internally to store array of `order` object as slice
 }
 
+// FIXME: There should be a better way of handling Order without stubbing
+//        Order inside a struct, similar to MarshalJSON via map[string]interface{}
 type order struct {
 	*Order `json:"order,omitempty"`
 }
-
-type orderArray []order
 
 func (o *Orders) UnmarshalJSON(b []byte) (err error) {
 	ordersStr := ""
@@ -46,7 +47,6 @@ func (o *Orders) UnmarshalJSON(b []byte) (err error) {
 
 	// If order is a string, i.e. "null"
 	if err = json.Unmarshal(b, &ordersStr); err == nil {
-		o.isNull = &ordersStr
 		return nil
 	}
 
@@ -68,8 +68,8 @@ func (o *Orders) UnmarshalJSON(b []byte) (err error) {
 
 func (o *Orders) MarshalJSON() ([]byte, error) {
 	// If order is null
-	if o.isNull != nil {
-		return json.Marshal(o.isNull)
+	if len(o.Order) == 0 {
+		return json.Marshal("null")
 	}
 
 	// If order array is size 1, return object directly
@@ -79,37 +79,6 @@ func (o *Orders) MarshalJSON() ([]byte, error) {
 		})
 	}
 
+	// Otherwise mashal as Orders normally, in this case using []Order
 	return json.Marshal(*o)
 }
-
-// func (o *Order) UnmarshalJSON(b []byte) (err error) {
-// 	orderObj := order{}
-//
-// 	log.Println(string(b))
-//
-// 	// If order is an object
-// 	if err = json.Unmarshal(b, &orderObj); err == nil {
-// 		log.Println("here")
-// 		*o = Order(orderObj)
-// 		return nil
-// 	}
-// 	//
-// 	// // If order is an array
-// 	// if err = json.Unmarshal(b, &orderArr); err == nil {
-// 	// 	o.orderArray = orderArr
-// 	// 	return nil
-// 	// } else {
-// 	// 	log.Println(err) // FIXME: Better error handling
-// 	// }
-//
-// 	return nil
-// }
-
-// func (o *Order) MarshalJSON() ([]byte, error) {
-//
-// 	if o.orderArray != nil {
-// 		return json.Marshal(o.orderArray)
-// 	}
-//
-// 	return json.Marshal(Order(*o))
-// }
